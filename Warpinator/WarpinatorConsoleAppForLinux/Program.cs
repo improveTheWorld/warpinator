48e7e1c093346c49d6c9b29c9781992c9742e8a5
Bilel GATRI <bilelgatri@gmail.com>
2022-09-16 18:35:19 +0200

    Add ConsoleApp for linux

---
+++ Warpinator/WarpinatorConsoleAppForLinux/Program.cs
@@ -0,0 +1,46 @@
+﻿using Grpc.Core;
+using iShare;
+using System.DirectoryServices.AccountManagement;
+using Server = iShare.Server;
+
+using Makaretu.Dns;
+using System.Resources;
+using Google.Protobuf.WellKnownTypes;
+
+
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+var netinterfaces = loadNetworkInterfacesList();
+
+
+Server server = new Server(new Settings { AutoAccept = true, GroupCode = "warpinator", NetworkInterface = netinterfaces.Keys.Last() });
+server.RemoteListUpdated += OnRemoteListUpdated;
+server.ServerUpdated += OnServerUpdated;
+//server.WatchByLogger();
+await server.Start();
+Console.ReadKey();
+
+//-------------------------- end of main
+void OnRemoteListUpdated(object s, EventArgs a)
+{ }
+
+void OnServerUpdated(object s, EventArgs a)
+{ }
+ 
+Dictionary<string, string>  loadNetworkInterfacesList()
+{
+    Dictionary<string, string> ifaceDict = new Dictionary<string, string>();
+    var ifaces = MulticastService.GetNetworkInterfaces();
+    ifaceDict.Clear();
+    ifaceDict.Add("", "any");   
+    int i = 1;
+    foreach (var iface in ifaces)
+    {
+        ifaceDict.Add(iface.Id, iface.Name);
+        
+        i++;
+    }
+    
+    return ifaceDict;
+}
